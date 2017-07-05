20df10560e9c1aa386df2d37ebec5373634dd73f
Onodera95 <uchkalec1995@mail.ru>
2017-07-05 21:52:13 +0300

    не получилось работать с классами

--- timer.php
+++ timer.php
@@ -1,20 +1,40 @@
 <?php
 
-class timer
+class Timer
 {
-       public $m;
-       public $h;
+    // public $m;
+    //public $h;
+
+    var $m;
+    var $h;
+    var $y;
+
+    function time()
+    {
+        $m = 15;
+        $h = 3;
+        $y = ($h + ($m / 60)) * 30 - $m * 6;
+        echo $y;
+    }
+}
+
+$time = new Timer;
+$time -> y = "";
+echo $time->y;
 
 
-        public function time() {
+
+
+        /**public function time() {
             $m = 15;
             $h = 3;
             $y = ($h + ($m / 60))*30 - $m * 6;
             echo $y;
-        }
+        } **/
 
 
-}
+
+
 
 
 ?>
